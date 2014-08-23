55f63d274b9390bc702b96c08989fe9bec0826a2
arcym <andrewmcp333@gmail.com>
2014-08-23 16:41:41 -0400

    Used keybinders for my double jumping.

--- game.js
+++ game.js
@@ -1,10 +1,10 @@
 var WIDTH = 640, HEIGHT = 480;
 
-var level, layer, player, stars, cursors, ui, score = 0;
+var level, layer, player, stars, cursors, overhead, score = 0;
 
 var jumpstate = {
 	height: 0,
-	again: 2
+	amount: 2
 };
 
 var game = new Phaser.Game(WIDTH, HEIGHT, Phaser.AUTO, "game",
@@ -40,8 +40,6 @@ var game = new Phaser.Game(WIDTH, HEIGHT, Phaser.AUTO, "game",
 		game.camera.follow(player);
 		//game.camera.y += 32*25;
 		
-		cursors = game.input.keyboard.createCursorKeys();
-		
 		stars = game.add.group();
 		stars.enableBody = true;
 		for(var i = 0; i < 60; i += 2)
@@ -51,76 +49,40 @@ var game = new Phaser.Game(WIDTH, HEIGHT, Phaser.AUTO, "game",
 			star.body.bounce.y = 0.7 + Math.random() * 0.2;
 		}
 		
-		ui = game.add.text(16, 16, "0", {fontSize: "32px", fill: "#000"});
-		ui.fixedToCamera = true;
+		overhead = game.add.text(16, 16, "0", {fontSize: "32px", fill: "#000"});
+		overhead.fixedToCamera = true;
+		
+		cursors = game.input.keyboard.createCursorKeys();
+		jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.UP);
+		jumpButton.onDown.add(function()
+		{
+			if(jumpstate.amount > 0)
+			{
+				jumpstate.amount -= 1;
+				player.body.velocity.y = 500 * -1;
+			}
+		});
 	},
 	
 	update: function()
 	{
 		game.physics.arcade.collide(player, layer);
 		game.physics.arcade.collide(stars, layer);
-		
-		player.body.velocity.x = 0;
-		
-		if(cursors.left.isDown)
-		{
-			player.body.velocity.x = -150;
-		}
-		else if(cursors.right.isDown)
-		{
-			player.body.velocity.x = 150;
-		}
-		
-		if(player.body.onFloor())
-		{
-			//jumpstate.height = 0;
-			jumpstate.amount = 2;
-		}
-		
-		if(player.body.velocity.y == 0)
-		{
-			if(game.input.keyboard.justPressed(Phaser.Keyboard.UP))
-			{
-				jumpstate.amount -= 1;
-				player.body.velocity.y = 500 * -1;
-			}
-		}
-		else if(player.body.velocity.y > 0)
-		{
-			if(game.input.keyboard.justPressed(Phaser.Keyboard.UP))
-			{
-				if(jumpstate.amount > 0)
-				{
-					jumpstate.amount -= 1;
-					player.body.velocity.y = 500 * -1;
-				}
-			}
-		}
-		
-		/*else if(cursors.up.isDown)
-		{
-			if(player.body.velocity.y < 0)
-			{
-				if(jumpstate.height < 5)
-				{
-					player.body.velocity.y -= JUMP_FORCE;
-					jumpstate.height++;
-				}
-			}
-		}*/
-		
 		game.physics.arcade.overlap(player, stars, function(player, star)
 		{
 			star.kill();
 			score += 10;
-		},
-		null, this);
+		});
 		
-		ui.text = player.body.velocity.y;
-	},
-	
-	render: function()
-	{
-		//game.debug.bodyInfo(player, 32, 320);
+		player.body.velocity.x = 0;
+		if(cursors.left.isDown)
+			player.body.velocity.x = -150;
+		else if(cursors.right.isDown)
+			player.body.velocity.x = 150;
+		
+		if(player.body.onFloor())
+			jumpstate.amount = 2;
+		
+		overhead.text = player.body.velocity.y;
 	}
 });
