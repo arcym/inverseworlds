40c81ce2ea9cf68a1afcb07633b3cd2df59ec008
Andrew McPherson <andrewmcp333@gmail.com>
2014-08-23 09:16:27 -0400

    Moved the camera to the bottom of the world.

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-var WIDTH = 704, HEIGHT = 480;
+var WIDTH = 640, HEIGHT = 480;
 
 var game = new Phaser.Game(WIDTH, HEIGHT, Phaser.AUTO, "game",
 {
@@ -12,9 +12,11 @@ var game = new Phaser.Game(WIDTH, HEIGHT, Phaser.AUTO, "game",
 	{
 		game.stage.backgroundColor = "#787878";
 		
-		var level = game.add.tilemap("level", 32, 32);
+		level = game.add.tilemap("level", 32, 32);
 		level.addTilesetImage("tiles", "tiles");
 		level.createLayer("FirstLayer").resizeWorld();
+		
+		game.camera.y += 32*25;
 	},
 	
 	update: function()
