4a651307d8b97c73292d23390fc2ec79d34edb3f
Andrew McPherson <andrewmcp333@gmail.com>
2015-02-03 19:12:52 -0500

    Began refactoring the code for browserifying.

--- source/index.js
+++ source/index.js
@@ -1,3 +1,9 @@
+var Phaser = require("phaser");
+var GameState = require("states/game.state");
+var TitleState = require("states/title.state");
+var Level = require("states/level.object");
+var Player = require("states/player.object");
+
 var level = new Level();
 var player = new Player();
 
